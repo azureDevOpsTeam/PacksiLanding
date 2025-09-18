fb385984fc1abc2a81f373807a02eccb116ab38a
AzureDevOpsTeam <azurepanel@yahoo.com>
2025-09-18 16:19:10 +0330

    add script ahref

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,7 @@ export default function RootLayout({
             __html: `!function(){var i="BcQr2J",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);} "complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();`,
           }}
         />
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="MpMbC2A8fgpbgGoQM04hLw" async></script>
       </head>
       <body className="antialiased">
         {children}
